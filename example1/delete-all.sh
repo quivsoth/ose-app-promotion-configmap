77a587a7177bc3d847f2655505b0f66c5c2cc126
quivsoth <phillip.knezevich@gmail.com>
2021-02-15 22:25:32 -0500

    updates for openshift 4 (tested)

--- example1/delete-all.sh
+++ example1/delete-all.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
 
 echo "Deleting the projects ...."
+# please add your OCP instance and user/password here
 oc login 10.2.2.2:8443 -u example -p password
+
+
 oc delete project/configmap-example
